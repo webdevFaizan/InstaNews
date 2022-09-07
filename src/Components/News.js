587e73f0886eb1ad02c974f2007f9ee693d15f89
webdevFaizan <webdev.faizan@gmail.com>
2022-09-07 11:57:47 +0530

    Loading bar on loading of the page or click of previous and next button has been integrated

--- src/Components/News.js
+++ src/Components/News.js
@@ -15,7 +15,7 @@ export default class News extends Component {
     super(props);     
     
     this.state = {      //In the function based component we used the setState react hook in order to change the state variable, here the state variable is not being changed by hook, but we will change the state using this.state.
-      loading : false,   
+      loading : true,   
       articles : [],
       pageNo: 1,
       totalResults : 0,
@@ -34,7 +34,7 @@ export default class News extends Component {
       articles : jsonData.articles,
       totalResults : jsonData.totalResults,
       maximumPages : maximumPages,
-      loading : true
+      loading : false
     });
   }
 
@@ -71,16 +71,15 @@ export default class News extends Component {
       <div className='container' style={{display : 'flex',flexDirection: 'column',justifyContent: 'space-around', border: '', alignItems: 'center'}}>
         <h1>Insta News - Top Headlines from all around the world.</h1>
         <div className="newsItemContainer" style={{display : 'flex', flexWrap: 'wrap', justifyContent: 'space-around', border: ''}}>
-          {/* {this.state.loading && <Loading />} */}
-          {this.state.articles.map((element)=>{
+          {this.state.loading && <Loading />}
+          {!this.state.loading && this.state.articles.map((element)=>{
             
             return <div className="newsCard" key={element.url}>
               <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />              
             </div>
           })}
         </div>
-          {/* {!this.state.loading &&  */}
-          {
+          {!this.state.loading &&           
               <div className='container my-3 mx-3' style={{display : 'flex', justifyContent : 'space-between'}}>
               <button disabled={this.state.pageNo<=1} type="button" className="btn btn-dark" onClick={this.onPreviousClick}>Previous</button>
               <button disabled={this.state.pageNo+1>=this.state.maximumPages} type="button" className="btn btn-dark" onClick={this.onNextClick}>Next</button>
